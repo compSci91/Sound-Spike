bbdece4b5056a7070f93c1eb338bec526035757a
Joshua Howell <howjosh@gmail.com>
2021-02-08 17:54:38 -0600

    JH -- Initial Commit -- Following instructions from a tutorial.

--- SoundSpike/ViewController.swift
+++ SoundSpike/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //https://www.youtube.com/watch?v=bZNAFkkUeKs&ab_channel=Devslopes
+    
+    @IBOutlet weak var totalPriceLbl: UILabel!
+    @IBOutlet weak var priceTxt: UITextField!
+    @IBOutlet weak var salesTaxTxt: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        totalPriceLbl.text = "" 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func calculateTotalPrice(_ sender: Any) {
+        let price:Double = Double(priceTxt.text!)!
+        let salesTax:Double = Double(salesTaxTxt.text!)!
+        
+        let totalSalesTax:Double = price * salesTax
+        let totalPrice = price + totalSalesTax
+        
+        totalPriceLbl.text = "$\(totalPrice)"
     }
 
 
