6f32ebe20d032d0bf2a6f645f79d603500415e8d
Joshua Howell <howjosh@gmail.com>
2021-02-08 18:20:10 -0600

    JH -- Play Sound -- Play Sound when you hit calculate.

--- SoundSpike/ViewController.swift
+++ SoundSpike/ViewController.swift
@@ -6,13 +6,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    //https://www.youtube.com/watch?v=bZNAFkkUeKs&ab_channel=Devslopes
+    
+    /*
+     Initial tutorial: https://www.youtube.com/watch?v=bZNAFkkUeKs&ab_channel=Devslopes
+     
+     Sound tutorial:
+     https://www.youtube.com/watch?v=2kflmGGMBOA&ab_channel=iOSAcademy
+     */
     
     @IBOutlet weak var totalPriceLbl: UILabel!
     @IBOutlet weak var priceTxt: UITextField!
     @IBOutlet weak var salesTaxTxt: UITextField!
+    
+    var player:AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +30,40 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculateTotalPrice(_ sender: Any) {
-        let price:Double = Double(priceTxt.text!)!
-        let salesTax:Double = Double(salesTaxTxt.text!)!
+//        let price:Double = Double(priceTxt.text!)!
+//        let salesTax:Double = Double(salesTaxTxt.text!)!
+//
+//        let totalSalesTax:Double = price * salesTax
+//        let totalPrice = price + totalSalesTax
+//
+//        totalPriceLbl.text = "$\(totalPrice)"
         
-        let totalSalesTax:Double = price * salesTax
-        let totalPrice = price + totalSalesTax
-        
-        totalPriceLbl.text = "$\(totalPrice)"
+        if let player = player, player.isPlaying{
+            //stop playback
+            player.stop()
+        }else {
+            //set up player, play
+            let urlString = Bundle.main.path(forResource: "tv-static-05", ofType: "mp3")
+            do {
+                try AVAudioSession.sharedInstance().setMode(.default)
+                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                
+                //need a url to the audio mp3
+                guard let urlString = urlString else {
+                    return
+                }
+                
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
+                
+                guard let player = player else {
+                    return
+                }
+                
+                player.play()
+            } catch {
+                print("Something went wrong!!!" )
+            }
+        }
     }
 
 
